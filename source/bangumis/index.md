ca81c8e165d7e2da7058d9087b475b15c9817e80
xingyuchen052-boop <xingyuchen052@gmail.com>
2026-04-21 19:10:51 +0800

    Fix website issues: avatar path, add bangumis page, and random post function

---
+++ source/bangumis/index.md
@@ -0,0 +1,5 @@
+---
+title: 追番页
+date: 2026-04-21 19:00:00
+type: bangumis
+---
--- source/js/random-post.js
+++ source/js/random-post.js
@@ -1,17 +1,17 @@
 // 随机文章功能
 function toRandomPost() {
   const posts = [
-    '/archives/',
-    '/categories/',
-    '/links/',
-    '/comments/',
-    '/music/',
-    '/bangumis/',
-    '/album/',
-    '/air-conditioner/',
-    '/about/',
-    '/essay/'
+    '/meloway-station/archives/',
+    '/meloway-station/categories/',
+    '/meloway-station/links/',
+    '/meloway-station/comments/',
+    '/meloway-station/music/',
+    '/meloway-station/bangumis/',
+    '/meloway-station/album/',
+    '/meloway-station/air-conditioner/',
+    '/meloway-station/about/',
+    '/meloway-station/essay/'
   ];
   const randomIndex = Math.floor(Math.random() * posts.length);
-  window.location.href = '/meloway-station' + posts[randomIndex];
+  window.location.href = posts[randomIndex];
 }
